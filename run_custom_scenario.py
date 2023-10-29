63cfd9bad5beffae04d8fa58c3ecb32efa3dac8b
ChrisRafnson <C24Christopher.Rafnson@afacademy.af.edu>
2023-10-29 17:55:22 -0600

    First Commit, can save SA pairs to CSV w/ rewards

--- run_custom_scenario.py
+++ run_custom_scenario.py
@@ -11,6 +11,9 @@ from airlift.envs.renderer import FlatRenderer
 from airlift.solutions.solutions import doepisode
 
 from solution.mysolution import MySolution
+from solution.mysolution import ShortestPath
+
+import pandas as pd
 
 """
 Create an AirliftEnv using all the generators. There exist multiple generators for one thing. For example instead of using the
@@ -21,7 +24,7 @@ DynamicCargoGenerator we can also use the StaticCargoGenerator.
 Uncomment the scenario below that you would like to use.
 """
 
-## A simple scenario with no dynamic events
+# A simple scenario with no dynamic events
 env = AirliftEnv(
     AirliftWorldGenerator(
         plane_types=[PlaneType(id=0, max_range=1.0, speed=0.05, max_weight=5)],
@@ -32,48 +35,64 @@ env = AirliftEnv(
                                                  processing_time=1,
                                                  working_capacity=100,
                                                  airports_per_unit_area=2),
-        route_generator=RouteByDistanceGenerator(route_ratio=2.5),
+        route_generator=RouteByDistanceGenerator(route_ratio=1.25),
         cargo_generator=StaticCargoGenerator(num_of_tasks=4,
                                              soft_deadline_multiplier=10,
                                              hard_deadline_multiplier=20),
-        airplane_generator=AirplaneGenerator(2),
+        airplane_generator=AirplaneGenerator(1),
     ),
     renderer=FlatRenderer(show_routes=True)
 )
 
-## A more complicated scenario with dynamic events
+# # A more complicated scenario with dynamic events
 # env = AirliftEnv(
 #     AirliftWorldGenerator(
 #         plane_types=[PlaneType(id=0, max_range=1.0, speed=0.05, max_weight=5)],
 #         airport_generator=RandomAirportGenerator(mapgen=PerlinMapGenerator(),
-#                                                  max_airports=20,
-#                                                  num_drop_off_airports=4,
-#                                                  num_pick_up_airports=4,
-#                                                  processing_time=4,
-#                                                  working_capacity=1,
-#                                                  airports_per_unit_area=2),
+#                                                  max_airports=80,
+#                                                  num_drop_off_airports=8,
+#                                                  num_pick_up_airports=8,
+#                                                  processing_time=8,
+#                                                  working_capacity=2,
+#                                                  airports_per_unit_area=8),
 #         route_generator=RouteByDistanceGenerator(malfunction_generator=EventIntervalGenerator(1 / 300, 200, 300),
-#                                                  route_ratio=2.5),
+#                                                  route_ratio=5),
 #         cargo_generator=DynamicCargoGenerator(cargo_creation_rate=1 / 100,
 #                                               soft_deadline_multiplier=4,
 #                                               hard_deadline_multiplier=12,
 #                                               num_initial_tasks=40,
 #                                               max_cargo_to_create=10),
-#         airplane_generator=AirplaneGenerator(10),
+#         airplane_generator=AirplaneGenerator(20),
 #     ),
 #     renderer=FlatRenderer(show_routes=True)
 # )
 
+master_df = pd.DataFrame()
+
 """
 Run a single episode utilizing the Solution we wrote with the above environment. 
 """
+MySolution.episode_num = 1
+my_solution = MySolution()
 env_info, metrics, time_taken, total_solution_time = \
   doepisode(env,
-            solution=MySolution(),
-            render=True,
+            solution=my_solution,
+            render=False,
             render_sleep_time=0, # Set this to 0.1 to slow down the simulation
             env_seed=100,
             solution_seed=200)
+
+
+
+#Now that the episode is finished, we backtrack and update the score for all out state action pairs
+condition = my_solution.df['episode_score'] == "TBD" 
+my_solution.df.loc[condition, 'episode_score'] = metrics.score
+master_df = pd.concat([master_df, my_solution.df], ignore_index=True)
+
+# Specify the csv file path
+csv_file_path = 'DirectEvaluationData.csv'
+# Export the DataFrame to a csv file
+master_df.to_csv(csv_file_path, index=False)
 
 print("Missed Deliveries: {}".format(metrics.missed_deliveries))
 print("Lateness:          {}".format(metrics.total_lateness))
